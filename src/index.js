c78cae82ba38cb2f2a464394613f780b314925e5
Rob Eisenberg <rob@bluespire.com>
2014-12-27 01:13:53 -0500

    feat(resources): enable plugin model

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-export {Compose} from './compose';
-export {If} from './if';
-export {Repeat} from './repeat';
-export {Show} from './show';
-export {SelectedItem} from './selected-item';
+import {Compose} from './compose';
+import {If} from './if';
+import {Repeat} from './repeat';
+import {Show} from './show';
+import {SelectedItem} from './selected-item';
+
+function install(aurelia){
+  aurelia.withResources([Show, If, Repeat, Compose, SelectedItem]);
+}
+
+export {
+  Compose,
+  If,
+  Repeat,
+  Show,
+  SelectedItem,
+  install
+};
