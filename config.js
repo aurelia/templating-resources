dac311acf54154a7f1022dabce3908008515b06f
Rob Eisenberg <rob@bluespire.com>
2015-01-07 10:50:51 -0500

    fix(package): update dependencies to latest

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ System.config({
   "map": {
     "aurelia-binding": "github:aurelia/binding@0.2.0",
     "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.2.1",
-    "aurelia-templating": "github:aurelia/templating@0.5.0",
+    "aurelia-templating": "github:aurelia/templating@0.6.0",
     "core-js": "npm:core-js@0.4.1",
     "github:aurelia/binding@0.2.0": {
       "aurelia-metadata": "github:aurelia/metadata@0.2.3",
@@ -26,7 +26,7 @@ System.config({
       "core-js": "npm:core-js@0.4.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     },
-    "github:aurelia/templating@0.5.0": {
+    "github:aurelia/templating@0.6.0": {
       "aurelia-binding": "github:aurelia/binding@0.2.0",
       "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.2.1",
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
