f63e023d022fbf32a57c2fbce388f72149e3662c
Rob Eisenberg <rob@bluespire.com>
2015-01-15 12:11:25 -0500

    fix(show): switch over to using a class instead of style manipulation

--- src/show.js
+++ src/show.js
@@ -1,5 +1,13 @@
 import {AttachedBehavior, Property} from 'aurelia-templating';
 
+function addStyleString(str) {
+  var node = document.createElement('style');
+  node.innerHTML = str;
+  document.body.appendChild(node);
+}
+
+addStyleString('.aurelia-hide { display:none; }');
+
 export class Show {
   static annotations(){
     return [
@@ -11,15 +19,13 @@ export class Show {
   static inject() { return [Element]; }
   constructor(element) {
     this.element = element;
-    this.displayStyle = element.style.display;
   }
 
   valueChanged(newValue){
-    if(newValue){
-      this.element.style.display = this.displayStyle || 'block';
-    }else{
-      this.displayStyle = this.element.style.display;
-      this.element.style.display = 'none';
+    if (newValue) {
+      this.element.classList.remove('aurelia-hide');
+    } else {
+      this.element.classList.add('aurelia-hide');
     }
   }
 }
