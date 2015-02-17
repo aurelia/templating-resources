a9a27a91777f749af1e7f9710340d711c8754601
Rob Eisenberg <rob@bluespire.com>
2015-02-17 15:46:58 -0500

    style(inner-html): fix ordering, braces, etc.

--- src/inner-html.js
+++ src/inner-html.js
@@ -3,38 +3,39 @@ import {Behavior} from 'aurelia-templating';
 export class InnerHTML {
   static metadata(){
     return Behavior
-    .attachedBehavior('inner-html')
-    .withOptions().and( x => {
-      x.withProperty('value', 'valueChanged');
-      x.withProperty('sanitizer');
-    });
+      .attachedBehavior('inner-html')
+      .withOptions().and( x => {
+        x.withProperty('value', 'valueChanged');
+        x.withProperty('sanitizer');
+      });
   }
 
-  static inject() { return [Element]; }
-  constructor(element) {
-    this.element = element;
-    this.sanitizer = InnerHTML.defaultSanitizer;
-  }
-
-  static defaultSanitizer(text) {
+  static defaultSanitizer(text){
     var SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
 
-    while (SCRIPT_REGEX.test(text)) {
-      text = text.replace(SCRIPT_REGEX, "");
+    while (SCRIPT_REGEX.test(text)){
+      text = text.replace(SCRIPT_REGEX, '');
     }
 
     return text;
   }
 
-  bind(bindingContext) {
+  static inject(){ return [Element]; }
+
+  constructor(element){
+    this.element = element;
+    this.sanitizer = InnerHTML.defaultSanitizer;
+  }
+
+  bind(){
     this.setElementInnerHTML(this.value);
   }
 
-  valueChanged(text){
-    this.setElementInnerHTML(text);
+  valueChanged(newValue){
+    this.setElementInnerHTML(newValue);
   }
 
-  setElementInnerHTML(text) {
+  setElementInnerHTML(text){
     text = this.sanitizer(text);
     this.element.innerHTML = text;
   }
