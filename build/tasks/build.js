3192d62d3f5f6a8f0dd4f5a1af564b3af32c30ff
Rob Eisenberg <rob@bluespire.com>
2015-11-16 03:06:57 -0500

    chore(all): d.ts generation

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -5,6 +5,25 @@ var paths = require('../paths');
 var compilerOptions = require('../babel-options');
 var assign = Object.assign || require('object.assign');
 
+var jsName = paths.packageName + '.js';
+
+gulp.task('build-index', function(){
+  var importsToAdd = [];
+
+  return gulp.src(paths.source)
+    .pipe(tools.sortFiles())
+    .pipe(through2.obj(function(file, enc, callback) {
+      file.contents = new Buffer(tools.extractImports(file.contents.toString("utf8"), importsToAdd));
+      this.push(file);
+      return callback();
+    }))
+    .pipe(concat(jsName))
+    .pipe(insert.transform(function(contents) {
+      return tools.createImportBlock(importsToAdd) + contents;
+    }))
+    .pipe(gulp.dest(paths.output));
+});
+
 gulp.task('build-es6', function () {
   return gulp.src(paths.source)
     .pipe(gulp.dest(paths.output + 'es6'));
@@ -28,10 +47,20 @@ gulp.task('build-system', function () {
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
+gulp.task('build-dts', function(){
+  return gulp.src(paths.root + paths.packageName + '.d.ts')
+    .pipe(gulp.dest(paths.output))
+    .pipe(gulp.dest(paths.output + 'es6'))
+    .pipe(gulp.dest(paths.output + 'commonjs'))
+    .pipe(gulp.dest(paths.output + 'amd'))
+    .pipe(gulp.dest(paths.output + 'system'));
+});
+
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
     ['build-es6', 'build-commonjs', 'build-amd', 'build-system'],
+    'build-dts',
     callback
   );
 });
