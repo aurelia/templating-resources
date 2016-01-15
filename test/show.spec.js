0bbf2becc62a54028fd26b15dea6e91e3a67ba70
Emil Einarsson <lootcher@gmail.com>
2016-01-15 18:40:32 +0100

    test(show): add test for show binding
    Added tests for show binding.

---
+++ test/show.spec.js
@@ -0,0 +1,38 @@
+import {Show} from '../src/show';
+
+describe('show', () => {
+  let sut, animator;
+
+  beforeEach(() => {
+    animator = new AnimatorMock();
+  });
+
+  it('should add aurelia-hide with animator.addClass when value is false', () => {
+    let target = document.createElement('input');
+
+    sut = new Show(target, animator);
+
+    spyOn(animator, 'addClass');
+
+    sut.valueChanged(false);
+
+    expect(animator.addClass).toHaveBeenCalledWith(target, 'aurelia-hide');
+  });
+
+  it('should remove aurelia-hide with animator.addClass when value is true', () => {
+    let target = document.createElement('input');
+
+    sut = new Show(target, animator);
+
+    spyOn(animator, 'removeClass');
+
+    sut.valueChanged(true);
+
+    expect(animator.removeClass).toHaveBeenCalledWith(target, 'aurelia-hide');
+  });
+});
+
+class AnimatorMock {
+  addClass() {}
+  removeClass() {}
+}
