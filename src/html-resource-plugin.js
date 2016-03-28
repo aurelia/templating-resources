8067068bb13a841de085f87471b448057b10c261
Jeremy Danyow <jdanyow@gmail.com>
2016-03-28 10:16:27 -0400

    refactor(html-resource-plugin): isolate element name logic

--- src/html-resource-plugin.js
+++ src/html-resource-plugin.js
@@ -1,6 +1,16 @@
 import {ViewEngine} from 'aurelia-templating';
 import {_createDynamicElement} from './dynamic-element';
 
+export function getElementName(address) {
+  let elementName = address.replace('.html', '');
+  let index = elementName.lastIndexOf('/');
+
+  if (index !== 0) {
+    elementName = elementName.substring(index + 1);
+  }
+  return elementName;
+}
+
 export function configure(config) {
   let viewEngine = config.container.get(ViewEngine);
   let loader = config.aurelia.loader;
@@ -9,12 +19,7 @@ export function configure(config) {
     'fetch': function(address) {
       return loader.loadTemplate(address).then(registryEntry => {
         let bindable = registryEntry.template.getAttribute('bindable');
-        let elementName = address.replace('.html', '');
-        let index = elementName.lastIndexOf('/');
-
-        if (index !== 0) {
-          elementName = elementName.substring(index + 1);
-        }
+        let elementName = getElementName(address);
 
         if (bindable) {
           bindable = bindable.split(',').map(x => x.trim());
---
+++ test/html-resource-plugin.spec.js
@@ -0,0 +1,12 @@
+import {getElementName} from '../src/html-resource-plugin';
+
+describe('html-resource-plugin', () => {
+  it('computes element name', () => {
+    expect(getElementName('foo.html')).toBe('foo');
+    expect(getElementName('/foo.html')).toBe('foo');
+    expect(getElementName('bar/foo.html')).toBe('foo');
+    expect(getElementName('foo.html?bar')).toBe('foo');
+    expect(getElementName('foo.html?bar.html')).toBe('foo');
+    expect(getElementName('https://bar/foo.html?bar.html')).toBe('foo');
+  });
+});
