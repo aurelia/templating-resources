3cc65d974cbfc0c7930fd7766f0d4b828de70965
Jeremy Danyow <jdanyow@gmail.com>
2016-04-14 21:33:22 -0400

    fix(analyze-view-factory): analyze type's view-factory
    fixes aurelia/framework#365

--- src/analyze-view-factory.js
+++ src/analyze-view-factory.js
@@ -7,10 +7,9 @@ export const lifecycleOptionalBehaviors = ['focus', 'if', 'repeat', 'show', 'wit
 function behaviorRequiresLifecycle(instruction) {
   let t = instruction.type;
   let name = t.elementName !== null ? t.elementName : t.attributeName;
-  if (lifecycleOptionalBehaviors.indexOf(name) === -1) {
-    return t.handlesAttached || t.handlesBind || t.handlesCreated || t.handlesDetached || t.handlesUnbind;
-  }
-  return instruction.viewFactory && viewsRequireLifecycle(instruction.viewFactory);
+  return lifecycleOptionalBehaviors.indexOf(name) === -1 && (t.handlesAttached || t.handlesBind || t.handlesCreated || t.handlesDetached || t.handlesUnbind)
+    || t.viewFactory && viewsRequireLifecycle(t.viewFactory)
+    || instruction.viewFactory && viewsRequireLifecycle(instruction.viewFactory);
 }
 
 function targetRequiresLifecycle(instruction) {
