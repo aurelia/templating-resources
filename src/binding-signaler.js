90f01142a8055e5930ed43c4ccf964e0d9ac5374
bigopon <bigopon.777@gmail.com>
2017-10-07 02:06:09 +1100

    refactor(Signal): use new signal mechanism

--- src/binding-signaler.js
+++ src/binding-signaler.js
@@ -1,16 +1,11 @@
-import {sourceContext} from 'aurelia-binding';
+import {sourceContext, signalBindings} from 'aurelia-binding';
+import * as LogManager from 'aurelia-logging';
 
 export class BindingSignaler {
-  signals = {};
 
   signal(name: string): void {
-    let bindings = this.signals[name];
-    if (!bindings) {
-      return;
-    }
-    let i = bindings.length;
-    while (i--) {
-      bindings[i].call(sourceContext);
-    }
+    LogManager.getLogger('binding-signaler')
+      .warn(`Binding signaler signal has been deprecated. Please use "import {signalBindings} from 'aurelia-binding' instead.`);
+    signalBindings(name);
   }
 }
