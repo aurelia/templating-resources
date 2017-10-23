ba827934ba56f3b8a97148df6a31f412eb30b8ba
EisenbergEffect <rob@bluespire.com>
2017-10-22 20:34:47 -0700

    chore(all): update build

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ System.config({
   },
 
   map: {
-    "aurelia-binding": "npm:aurelia-binding@1.4.0",
+    "aurelia-binding": "npm:aurelia-binding@1.5.0",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-rc.1.0.1",
     "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0",
@@ -54,13 +54,13 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
-    "npm:aurelia-binding@1.2.0": {
+    "npm:aurelia-binding@1.4.0": {
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
       "aurelia-pal": "npm:aurelia-pal@1.4.0",
       "aurelia-task-queue": "npm:aurelia-task-queue@1.2.1"
     },
-    "npm:aurelia-binding@1.4.0": {
+    "npm:aurelia-binding@1.5.0": {
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
       "aurelia-metadata": "npm:aurelia-metadata@1.0.3",
       "aurelia-pal": "npm:aurelia-pal@1.4.0",
@@ -90,7 +90,7 @@ System.config({
       "aurelia-logging": "npm:aurelia-logging@1.3.1"
     },
     "npm:aurelia-framework@1.0.0-rc.1.0.13": {
-      "aurelia-binding": "npm:aurelia-binding@1.4.0",
+      "aurelia-binding": "npm:aurelia-binding@1.5.0",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
@@ -140,12 +140,12 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.4.0"
     },
     "npm:aurelia-templating-binding@1.0.0-rc.1.0.1": {
-      "aurelia-binding": "npm:aurelia-binding@1.4.0",
+      "aurelia-binding": "npm:aurelia-binding@1.5.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
       "aurelia-templating": "npm:aurelia-templating@1.3.0"
     },
     "npm:aurelia-templating-resources@1.0.0-rc.1.0.2": {
-      "aurelia-binding": "npm:aurelia-binding@1.4.0",
+      "aurelia-binding": "npm:aurelia-binding@1.5.0",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
@@ -165,7 +165,7 @@ System.config({
       "aurelia-templating": "npm:aurelia-templating@1.3.0"
     },
     "npm:aurelia-templating@1.3.0": {
-      "aurelia-binding": "npm:aurelia-binding@1.4.0",
+      "aurelia-binding": "npm:aurelia-binding@1.5.0",
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.3.0",
       "aurelia-loader": "npm:aurelia-loader@1.0.0",
       "aurelia-logging": "npm:aurelia-logging@1.3.1",
