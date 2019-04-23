3607bf38e510036cdad4c0ba9c0572e9166d8cfd
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-04-23 23:37:15 +1000

    build(all): move build to rollup

---
+++ rollup.config.js
@@ -0,0 +1,76 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default [
+  {
+    input: 'src/aurelia-templating-resources.ts',
+    output: [
+      {
+        file: 'dist/es2015/aurelia-templating-resources.js',
+        format: 'esm'
+      }
+    ],
+    plugins: [
+      typescript({
+        cacheRoot: '.rollupcache',
+        tsconfigOverride: {
+          compilerOptions: {
+            removeComments: true,
+          }
+        }
+      })
+    ]
+  },
+  {
+    input: 'src/aurelia-templating-resources.ts',
+    output: [{
+      file: 'dist/es2017/aurelia-templating-resources.js',
+      format: 'esm'
+    }],
+    plugins: [
+      typescript({
+        cacheRoot: '.rollupcache',
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'es2017',
+            removeComments: true,
+          }
+        }
+      })
+    ]
+  },
+  {
+    input: 'src/aurelia-templating-resources.ts',
+    output: [
+      { file: 'dist/amd/aurelia-templating-resources.js', format: 'amd', id: 'aurelia-templating-resources' },
+      { file: 'dist/commonjs/aurelia-templating-resources.js', format: 'cjs' },
+      { file: 'dist/system/aurelia-templating-resources.js', format: 'system' },
+      { file: 'dist/native-modules/aurelia-templating-resources.js', format: 'esm' },
+    ],
+    plugins: [
+      typescript({
+        cacheRoot: '.rollupcache',
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'es5',
+            removeComments: true,
+          }
+        }
+      })
+    ]
+  }
+].map(config => {
+  config.external = [
+    'aurelia-binding',
+    'aurelia-dependency-injection',
+    'aurelia-pal',
+    'aurelia-templating',
+    'aurelia-templating-resources',
+    'aurelia-task-queue',
+    'aurelia-logging',
+    'aurelia-path',
+    'aurelia-loader',
+    'aurelia-metadata'
+  ];
+  config.output.forEach(output => output.sourcemap = true);
+  return config;
+});
