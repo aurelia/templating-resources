70d21f25df38c7c1ba101bbc03f000cab796aba3
bigopon.777@gmail.com <bigopon.777@gmail.com>
2019-04-25 07:35:47 +1000

    refactor(signal): more minification friendly code

--- src/signal-binding-behavior.ts
+++ src/signal-binding-behavior.ts
@@ -16,16 +16,17 @@ export class SignalBindingBehavior {
     if (!binding.updateTarget) {
       throw new Error('Only property bindings and string interpolation bindings can be signaled.  Trigger, delegate and call bindings cannot be signaled.');
     }
+    let signals = this.signals;
     if (names.length === 1) {
       let name = names[0];
-      let bindings = this.signals[name] || (this.signals[name] = []);
+      let bindings = signals[name] || (signals[name] = []);
       bindings.push(binding);
       binding.signalName = name;
     } else if (names.length > 1) {
       let i = names.length;
       while (i--) {
         let name = names[i];
-        let bindings = this.signals[name] || (this.signals[name] = []);
+        let bindings = signals[name] || (signals[name] = []);
         bindings.push(binding);
       }
       binding.signalName = names;
@@ -35,6 +36,7 @@ export class SignalBindingBehavior {
   }
 
   unbind(binding, source) {
+    let signals = this.signals;
     let name = binding.signalName;
     binding.signalName = null;
     if (Array.isArray(name)) {
@@ -42,11 +44,11 @@ export class SignalBindingBehavior {
       let i = names.length;
       while (i--) {
         let n = names[i];
-        let bindings = this.signals[n];
+        let bindings = signals[n];
         bindings.splice(bindings.indexOf(binding), 1);
       }
     } else {
-      let bindings = this.signals[name];
+      let bindings = signals[name];
       bindings.splice(bindings.indexOf(binding), 1);
     }
   }
