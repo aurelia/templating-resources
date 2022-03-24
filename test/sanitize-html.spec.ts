0ed95422c1441753194a5101810a5968a838439f
bigopon <bigopon.777@gmail.com>
2022-03-25 00:57:16 +1100

    feat(sanitizer): throw instead

--- test/sanitize-html.spec.ts
+++ test/sanitize-html.spec.ts
@@ -9,21 +9,8 @@ describe('SanitizeHtmlValueConverter', () => {
     converter = new SanitizeHTMLValueConverter(new HTMLSanitizer());
   });
 
-  it('defaultSanitizer should remove script tags', () => {
-    let a = '<script src="http://www.evil.org"></script>',
-        b = '<div><script src="http://www.evil.org"></script></div>',
-        c = 'foo <script src="http://www.evil.org"></script> bar',
-        d = '<div></div>',
-        e = 'foo bar';
-
-    expect(converter.toView('')).toBe('');
-    expect(converter.toView(null)).toBe(null);
-    expect(converter.toView(undefined)).toBe(null);
-    expect(converter.toView(a)).toBe('');
-    expect(converter.toView(b)).toBe('<div></div>');
-    expect(converter.toView(c)).toBe('foo  bar');
-    expect(converter.toView(d)).toBe('<div></div>');
-    expect(converter.toView(e)).toBe('foo bar');
+  it('defaultSanitizer should throw', () => {
+    expect(() => converter.toView('')).toThrow();
   });
 
   it('custom sanitizers can be used', () => {
